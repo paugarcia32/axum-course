f6629b95b7d957b95f621497e41f22338ce27e0c
“paugarcia32” <paugarcia32@gmail.com>
2023-07-11 14:22:28 +0200

    extracting a string from body

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,14 @@
 mod hello_world;
-use axum::{routing::patch, Router};
+mod mirror_body_string;
+use axum::{
+    routing::{patch, post},
+    Router,
+};
 use hello_world::hello_world;
+use mirror_body_string::mirror_body_string;
 
 pub fn create_routes() -> Router {
-    Router::new().route("/", patch(hello_world))
+    Router::new()
+        .route("/", patch(hello_world))
+        .route("/mirror_body_string", post(mirror_body_string))
 }
