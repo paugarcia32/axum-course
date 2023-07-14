3a74164dcd0c86efba20fe195051a24dc7d3e5a9
paugarcia32 <paugarcia32@gmail.com>
2023-07-14 23:41:50 +0200

    query params

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,6 +2,7 @@ mod hello_world;
 mod mirror_body_json;
 mod mirror_body_string;
 mod path_variables;
+mod query_params;
 use axum::{
     handler,
     routing::{get, patch, post},
@@ -11,6 +12,7 @@ use hello_world::hello_world;
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
 use path_variables::{hard_coded_path, path_variables};
+use query_params::query_params;
 pub fn create_routes() -> Router {
     Router::new()
         .route("/", patch(hello_world))
@@ -18,4 +20,5 @@ pub fn create_routes() -> Router {
         .route("/mirror_body_json", post(mirror_body_json))
         .route("/path_variables/:id", get(path_variables))
         .route("/path_variables/15", get(hard_coded_path))
+        .route("/query_params", get(query_params))
 }
