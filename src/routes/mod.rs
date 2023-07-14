a41649d755049e24d82aa65a25466e87d1778180
paugarcia32 <paugarcia32@gmail.com>
2023-07-14 19:58:29 +0200

    Extracting JSON

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,14 +1,18 @@
 mod hello_world;
+mod mirror_body_json;
 mod mirror_body_string;
 use axum::{
+    handler,
     routing::{patch, post},
     Router,
 };
 use hello_world::hello_world;
+use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
 
 pub fn create_routes() -> Router {
     Router::new()
         .route("/", patch(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
+        .route("/mirror_body_json", post(mirror_body_json))
 }
