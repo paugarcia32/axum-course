0ab2132ef4c16615bb72cfb1fc06c64b7e6f3136
paugarcia32 <paugarcia32@gmail.com>
2023-07-15 20:09:18 +0200

    extract standard headers

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,18 +1,20 @@
 mod hello_world;
 mod mirror_body_json;
 mod mirror_body_string;
+mod mirror_user_agent;
 mod path_variables;
 mod query_params;
 use axum::{
-    handler,
     routing::{get, patch, post},
     Router,
 };
 use hello_world::hello_world;
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
+use mirror_user_agent::mirror_user_agent;
 use path_variables::{hard_coded_path, path_variables};
 use query_params::query_params;
+
 pub fn create_routes() -> Router {
     Router::new()
         .route("/", patch(hello_world))
@@ -21,4 +23,5 @@ pub fn create_routes() -> Router {
         .route("/path_variables/:id", get(path_variables))
         .route("/path_variables/15", get(hard_coded_path))
         .route("/query_params", get(query_params))
+        .route("/mirror_user_agent", get(mirror_user_agent))
 }
