f5daca0f8f3f7d08f5f92bd42b0c5002934cca01
paugarcia32 <paugarcia32@gmail.com>
2023-07-17 23:48:45 +0200

    200 HTTP codes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -8,6 +8,7 @@ mod mirror_user_agent;
 mod path_variables;
 mod query_params;
 mod read_middleware_custom_header;
+mod returns_201;
 mod set_middleware_custom_heaader;
 use always_errors::always_errors;
 use axum::{
@@ -25,6 +26,7 @@ use mirror_user_agent::mirror_user_agent;
 use path_variables::{hard_coded_path, path_variables};
 use query_params::query_params;
 use read_middleware_custom_header::read_middleware_custom_header;
+use returns_201::returns_201;
 use set_middleware_custom_heaader::set_middleware_custom_heaader;
 use tower_http::cors::{Any, CorsLayer};
 
@@ -60,4 +62,5 @@ pub fn create_routes() -> Router {
         .layer(cors)
         .layer(Extension(shared_data))
         .route("/always_errors", get(always_errors))
+        .route("/returns_201", post(returns_201))
 }
